e29e82927c46254e7016b6008d9ebc1159fa6993
Victor Kamrowski <vkamrowski50@stuy.edu>
2025-07-30 23:29:33 -0400

    fixed small errors. added try except, elif, proper format

--- linear_regression.py
+++ linear_regression.py
@@ -18,22 +18,34 @@ class LinearRegression():
             choice = input("Choose (1-5): ")
 
             if choice == 1:
-                x,y = input("Input a data point in the exact form 'x,y' (x,y both ints):")
-                if type(x) != int or type(y) != int:
-                    print("Invalid inputs. Must be in form 'x,y' with x and y being ints")
-                    continue
-                self.inputs.append(x)
-                self.outputs.append(y)
-                print("Values inputted into data set")
-            if choice == 2:
+                try:
+                    x_str,y_str = input("Input a data point in the exact form 'x,y' (x,y both ints): ").split(",")
+                    x = int(x_str)
+                    y = int(y_str)
+                    self.inputs.append(x)
+                    self.outputs.append(y)
+                    print("Values inputted into data set")
+                except:
+                    print("Invalid format. Must be 'x,y' with both as ints.")
+
+            elif choice == 2:
+                
                 #implement gradient descent
-            if choice == 3:
-                x_value = input("Enter an int in form 'x': ")
-                predicted_value = self.slope * x_value + self.y_intercept
-                print("Predicted output: {predicted_value}")
-            if choice == 4:
-                print("Current linear equation: y = {self.m}x + {self.y_intercept}")
-            if choice == 5:
+
+            elif choice == 3:
+                try:
+                    x_value = int(input("Enter an int in form 'x': "))
+                    predicted_value = self.slope * x_value + self.y_intercept
+                    print(f"Predicted output: {predicted_value}")
+                except:
+                    print("Invalid input, must be an int.")
+
+            elif choice == 4:
+                print(f"Current linear equation: y = {self.slope}x + {self.y_intercept}")
+            
+            elif choice == 5:
                 print("Exiting...")
                 break
             
+            else:
+                print("Invalid choice. Enter a number from 1 to 5")
